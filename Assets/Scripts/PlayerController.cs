802bd39b10c72be3178f1f568ea3da9b99baeccf
John Simmons <simionescuionut1@gmail.com>
2026-07-05 19:28:19 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveCooldown = 0.15f;
+
+    private Vector2Int gridPos;
+    private float timer;
+    public Tilemap wallTilemap;
+    public Tilemap treasureTilemap;
+
+    
+    void Start()
+    {
+        // Convert world position → grid position correctly
+        gridPos = new Vector2Int(
+            Mathf.FloorToInt(transform.position.x),
+            Mathf.FloorToInt(transform.position.y)
+        );
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer < moveCooldown)
+            return;
+
+        Vector2Int dir = Vector2Int.zero;
+
+        if (Input.GetKeyDown(KeyCode.W)) dir = Vector2Int.up;
+        if (Input.GetKeyDown(KeyCode.S)) dir = Vector2Int.down;
+        if (Input.GetKeyDown(KeyCode.A)) dir = Vector2Int.left;
+        if (Input.GetKeyDown(KeyCode.D)) dir = Vector2Int.right;
+
+        if (dir != Vector2Int.zero)
+        {
+            Vector2Int target = gridPos + dir;
+
+            Vector3Int targetCell = new Vector3Int(target.x, target.y, 0);
+
+            // If there's a wall, cancel movement
+            if (wallTilemap.GetTile(targetCell) != null)
+            {
+                return;
+            }
+            
+            //move player
+            gridPos = target;
+            transform.position = new Vector3(gridPos.x + 0.5f, gridPos.y + 0.5f, 0f);
+            
+             // treasure check (after move)
+            Vector3Int currentCell = new Vector3Int(gridPos.x, gridPos.y, 0);
+
+            Debug.Log("Checking treasure at: " + currentCell);
+            Debug.Log("Treasure Tilemap assigned? " + (treasureTilemap != null));
+            
+            if (treasureTilemap != null && treasureTilemap.HasTile(currentCell))
+            {
+            treasureTilemap.SetTile(currentCell, null);
+            Debug.Log("Picked up treasure!");
+            }
+            //cooldown reset
+            timer = 0f;
+        }
+    }
+}
